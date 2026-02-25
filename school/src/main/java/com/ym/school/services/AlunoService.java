1f099f310b35ca8d65ba2c615a5e3164eb6dffb7
Guilherme <japajao25@gmail.com>
2026-02-25 20:10:46 -0300

    service criado

---
+++ school/src/main/java/com/ym/school/services/AlunoService.java
@@ -0,0 +1,29 @@
+package com.ym.school.services;
+
+import com.ym.school.models.AlunoModel;
+import com.ym.school.repositories.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    public AlunoModel criarAluno(AlunoModel alunoModel){
+        return alunoRepository.save(alunoModel);
+    }
+
+    public List<AlunoModel> buscarTodosAlunos(){
+        return alunoRepository.findAll();
+    }
+
+    public void deletarAluno(Long id){
+        alunoRepository.deleteById(id);
+    }
+
+
+}
